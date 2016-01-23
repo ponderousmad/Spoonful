e83afa72d2b7397aa01eac45e325024e036ce688
Adrian Smith <agnomen@gmail.com>
2016-01-23 09:27:34 -0500

    Clone flipbook from Opdozitz.

---
+++ src/flipbook.js
@@ -0,0 +1,33 @@
+﻿var Flipbook = (function () {
+    function Flipbook(imageBatch, baseName, frameCount, digits) {
+        this.frames = [];
+        for (var i = 1; i <= frameCount; ++i) {
+            var number = i.toString();
+            while (number.length < digits) {
+                number = "0" + number;
+            }
+            this.frames.push(imageBatch.load(baseName + number + ".png"));
+        }
+    }
+    
+    Flipbook.prototype.setupPlayback = function(frameTime) {
+        return {
+            elapsed: 0,
+            timePerFrame: frameTime
+        };
+    };
+    
+    Flipbook.prototype.updatePlayback = function(elapsed, playback) {
+        playback.elapsed += elapsed;
+        return playback.elapsed > (playback.timePerFrame * this.frames.length);
+    };
+    
+    Flipbook.prototype.draw = function(context, playback, location, width, height, center) {
+        var index = Math.min(this.frames.length - 1, Math.floor(playback.elapsed / playback.timePerFrame)),
+            x = location.x - (center ? width * 0.5 : 0),
+            y = location.y - (center ? height * 0.5 : 0);
+        context.drawImage(this.frames[index], x, y, width, height);
+    };
+    
+    return Flipbook;
+}());
